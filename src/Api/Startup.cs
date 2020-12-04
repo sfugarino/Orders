35bcc8e73588653c44aa6f8b690eff559a2e23de
Samuel Fugarino <sfugarino@msn.com>
2020-12-04 04:24:39 -0500

    Updated swagger url

--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -40,9 +40,14 @@ namespace api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
+
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Orders API");
+                // c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
